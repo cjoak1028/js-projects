3f69c90e078a29326b6bad573b4a34d884444456
David Kim <cjoak1028@gmail.com>
2021-04-08 03:41:06 -0400

    fixed time display

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -4,7 +4,7 @@ const minsElement = document.getElementById('minutes');
 const secElement = document.getElementById('seconds');
 
 
-const newYear = '1 Jan 2022';
+const newYear = '01 Jan 2022';
 
 const countdown = () => {
     const newYearDate = new Date(newYear);
@@ -17,10 +17,33 @@ const countdown = () => {
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    daysElement.innerHTML = days;
-    hrsElement.innerHTML = hours;
-    minsElement.innerHTML = minutes;
-    secElement.innerHTML = seconds;
+    daysElement.innerHTML = formatDays(days);
+    hrsElement.innerHTML = formatTime(hours);
+    minsElement.innerHTML = formatTime(minutes);
+    secElement.innerHTML = formatTime(seconds);
+}
+
+const formatTime = (time) => {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+const formatDays = (days) => {
+    // switch (days) {
+    //     case (days < 10):
+    //         console.log('hello')
+    //         return (`00${days}`);
+    //     case (days < 100):
+    //         return (`0${days}`);
+    //     case (days >= 100):
+    //         return days;
+    // }
+    if (days < 10) {
+        return (`00${days}`);
+    } else if (days < 100) {
+        return (`0${days}`);
+    } else {
+        return days;
+    }
 }
 
 countdown();
