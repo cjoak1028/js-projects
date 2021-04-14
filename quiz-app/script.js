bd757cf3690bbcab1e10ad471cf4d3e495767d18
David Kim <cjoak1028@gmail.com>
2021-04-13 23:47:34 -0400

    created quiz questions

---
+++ quiz-app/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: 'What does HTML stand for?',
+        a: 'How To Make Love',
+        b: 'Hit The Mother Load',
+        c: 'HyperText Markup Language',
+        d: 'None of the above',
+        answer: 'c'
+    },
+    {
+        question: 'Which programming language was most popular in 2019?',
+        a: 'Python',
+        b: 'JavaScript',
+        c: 'C',
+        d: 'Java',
+        answer: 'd'
+    },
+    {
+        question: 'What year was JavaScript launched?',
+        a: '1994',
+        b: '2000',
+        c: '2002',
+        d: 'None of the above',
+        answer: 'd'
+    },
+    {
+        question: 'Which front-end JavaScript framework is most in demand currently?',
+        a: 'Vue',
+        b: 'React',
+        c: 'Angular',
+        d: 'Svelte',
+        answer: 'b'
+    }
+]
